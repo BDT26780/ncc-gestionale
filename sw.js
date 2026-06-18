073addb5d44d1de6aab96e6987566bb20cbddd7c
Ruth Esther Garcia Murray <RuthyMurray@192.168.0.118>
2026-06-18 16:50:15 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,25 +1,18 @@
-const CACHE = "bdt-ncc-v3";
+const CACHE = "bdt-ncc-v4";
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then((cache) =>
-      cache.addAll(["/ncc-gestionale/", "/ncc-gestionale/index.html"])
-    )
-  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+      Promise.all(keys.map((k) => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
