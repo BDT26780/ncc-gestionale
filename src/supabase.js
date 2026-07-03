c2e2b5d344eaa77dc7aec899ff657082ad4e8e44
Ruth Esther Garcia Murray <RuthyMurray@192.168.0.118>
2026-07-03 18:17:38 +0200

    rimuovi workflow Actions

---
+++ src/supabase.js
@@ -0,0 +1,4 @@
+import{createClient}from"@supabase/supabase-js";
+const SUPA_URL="https://uqafkwmtwlooiituspas.supabase.co";
+const SUPA_KEY="sb_publishable_kJo4kjKyp7KaiT3v0fFRqQ_IPP_GuW7";
+export const supa=createClient(SUPA_URL,SUPA_KEY);
