e7e0212afda13f1d76e0100751a5a5971fdb0d7d
Omar <blackdiamondtransfert@gmail.com>
2026-07-23 20:54:57 +0200

    deploy

--- sw.js
+++ sw.js
@@ -1,18 +1,15 @@
-const CACHE = "bdt-ncc-v6";
-
+const CACHE = "bdt-ncc-v7";
 self.addEventListener("install", (e) => {
   self.skipWaiting();
 });
-
 self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.map((k) => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
-
 self.addEventListener("fetch", (e) => {
-  e.respondWith(fetch(e.request));
+  if(e.request.method !== "GET") return;
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
